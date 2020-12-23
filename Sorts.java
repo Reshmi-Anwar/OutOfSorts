828ba59de9f6b415085623eeffdf059a5e83b7b6
Reshmi-Anwar <ranwar10@stuy.edu>
2020-12-23 18:52:11 -0500

    fixed bugs

--- Sorts.java
+++ Sorts.java
@@ -14,6 +14,31 @@ public class Sorts{
   }
 
   public static void selectionSort(int[] data){
-    
+    for(int i = 0; i < data.length - 1; i++){
+      int index = i;
+      for(int j = i; j < data.length; j++){
+        if(data[j] < data[index]){
+          index = j;
+        }
+      }
+      int swap = data[i];
+      data[i] = data[index];
+      data[index] = swap;
+    }
+  }
+
+  public static void insertionSort(int[] data){
+    for(int i = 1; i < data.length; i++){
+      int x = data[i];
+      int y = i - 1;
+      while(x < data[y]){
+        data[y + 1] = data[y];
+        y--;
+        if(y==-1){
+          break;
+        }
+      }
+      data[y + 1] = x;
+    }
   }
 }
